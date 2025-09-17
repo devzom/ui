bb9c09e0e23467164f79d54ac51aa52ed4a85ccf
Hugo Richard <hugo.richard@epitech.eu>
2025-09-17 14:45:53 +0100

    feat(ProseImg): improve zoom functionality

--- src/theme/prose/img.ts
+++ src/theme/prose/img.ts
@@ -2,7 +2,8 @@ export default {
   slots: {
     base: 'rounded-md',
     overlay: 'fixed inset-0 bg-default/75 backdrop-blur-sm will-change-opacity',
-    content: 'fixed inset-0 flex items-center justify-center cursor-zoom-out focus:outline-none p-4 sm:p-8'
+    content: 'fixed inset-0 flex items-center justify-center cursor-zoom-out focus:outline-none',
+    zoomedImage: 'w-auto sm:h-full max-w-[90vw] max-h-[90vh] rounded-md'
   },
   variants: {
     zoom: {
